c5f092e95fd22a96aebcf54f4e1dfdef3a16659d
Shoumitro Ray <sray@idare.io>
2023-09-01 12:39:15 +0600

    customer crud added

--- customers/urls.py
+++ customers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from customers.views import CustomerList
-
+from customers.views import CustomerList, CustomerDetailView
 
 urlpatterns = [
     path('customers/', CustomerList.as_view(), name='customer-list'),
+    path('customers/<int:pk>/', CustomerDetailView.as_view(), name='customer-detail'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -3,6 +3,12 @@ from customers.models import Customer
 from customers.serializers import CustomerSerializer
 
 
-class CustomerList(generics.ListAPIView):
+class CustomerList(generics.ListCreateAPIView):
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
+
+
+class CustomerDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
